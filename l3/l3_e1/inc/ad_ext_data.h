fbca8c27e485f3ac0f7b3d2e615e06c8532ee87c
AliNordic <ali.aljaani@nordicsemi.no>
2026-03-13 15:32:37 +0100

    Extended advertising full payload (without chaining)

---
+++ l3/l3_e1/inc/ad_ext_data.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef AD_EXT_DATA_H
+#define AD_EXT_DATA_H
+
+#include <zephyr/bluetooth/bluetooth.h>
+
+extern const struct bt_data ad_ext[];
+extern const size_t ad_ext_size;
+
+#endif /* AD_EXT_DATA_H */
